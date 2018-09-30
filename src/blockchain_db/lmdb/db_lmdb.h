d5b83a235bebe20b4cc716afd0c817ea6b4b2ed8
LINE <769483043@qq.com>
2018-09-30 15:24:44 +0800

    init

---
+++ src/blockchain_db/lmdb/db_lmdb.h
@@ -0,0 +1,116 @@
+#include <stdint.h>
+#include "lmdb.h"
+#include "glib.h"
+
+typedef struct mdb_txn_cursors
+{
+  MDB_cursor *m_txc_blocks;
+  MDB_cursor *m_txc_block_heights;
+  MDB_cursor *m_txc_block_info;
+
+  MDB_cursor *m_txc_output_txs;
+  MDB_cursor *m_txc_output_amounts;
+
+  MDB_cursor *m_txc_txs;
+  MDB_cursor *m_txc_txs_pruned;
+  MDB_cursor *m_txc_txs_prunable;
+  MDB_cursor *m_txc_txs_prunable_hash;
+  MDB_cursor *m_txc_tx_indices;
+  MDB_cursor *m_txc_tx_outputs;
+
+  MDB_cursor *m_txc_spent_keys;
+
+  MDB_cursor *m_txc_txpool_meta;
+  MDB_cursor *m_txc_txpool_blob;
+
+  MDB_cursor *m_txc_hf_versions;
+} mdb_txn_cursors;
+
+#define m_cur_blocks	m_cursors->m_txc_blocks
+#define m_cur_block_heights	m_cursors->m_txc_block_heights
+#define m_cur_block_info	m_cursors->m_txc_block_info
+#define m_cur_output_txs	m_cursors->m_txc_output_txs
+#define m_cur_output_amounts	m_cursors->m_txc_output_amounts
+#define m_cur_txs	m_cursors->m_txc_txs
+#define m_cur_txs_pruned	m_cursors->m_txc_txs_pruned
+#define m_cur_txs_prunable	m_cursors->m_txc_txs_prunable
+#define m_cur_txs_prunable_hash	m_cursors->m_txc_txs_prunable_hash
+#define m_cur_tx_indices	m_cursors->m_txc_tx_indices
+#define m_cur_tx_outputs	m_cursors->m_txc_tx_outputs
+#define m_cur_spent_keys	m_cursors->m_txc_spent_keys
+#define m_cur_txpool_meta	m_cursors->m_txc_txpool_meta
+#define m_cur_txpool_blob	m_cursors->m_txc_txpool_blob
+#define m_cur_hf_versions	m_cursors->m_txc_hf_versions
+
+typedef struct mdb_rflags
+{
+  bool m_rf_txn;
+  bool m_rf_blocks;
+  bool m_rf_block_heights;
+  bool m_rf_block_info;
+  bool m_rf_output_txs;
+  bool m_rf_output_amounts;
+  bool m_rf_txs;
+  bool m_rf_txs_pruned;
+  bool m_rf_txs_prunable;
+  bool m_rf_txs_prunable_hash;
+  bool m_rf_tx_indices;
+  bool m_rf_tx_outputs;
+  bool m_rf_spent_keys;
+  bool m_rf_txpool_meta;
+  bool m_rf_txpool_blob;
+  bool m_rf_hf_versions;
+} mdb_rflags;
+
+typedef struct mdb_threadinfo
+{
+  MDB_txn *m_ti_rtxn;	// per-thread read txn
+  mdb_txn_cursors m_ti_rcursors;	// per-thread read cursors
+  mdb_rflags m_ti_rflags;	// per-thread read state
+
+  ~mdb_threadinfo();
+} mdb_threadinfo;
+
+
+struct mdb_txn_safe
+{
+  mdb_txn_safe(const bool check=true);
+  ~mdb_txn_safe();
+
+  void commit(char* message = "");
+
+  // This should only be needed for batch transaction which must be ensured to
+  // be aborted before mdb_env_close, not after. So we can't rely on
+  // BlockchainLMDB destructor to call mdb_txn_safe destructor, as that's too late
+  // to properly abort, since mdb_env_close would have been called earlier.
+  void abort();
+  void uncheck();
+
+  operator MDB_txn*()
+  {
+    return m_txn;
+  }
+
+  operator MDB_txn**()
+  {
+    return &m_txn;
+  }
+
+  uint64_t num_active_tx() const;
+
+  static void prevent_new_txns();
+  static void wait_no_active_txns();
+  static void allow_new_txns();
+
+  mdb_threadinfo* m_tinfo;
+  MDB_txn* m_txn;
+  bool m_batch_txn = false;
+  bool m_check;
+  static sig_atomic_t num_active_txns;
+  // static std::atomic<uint64_t> num_active_txns;
+
+  // could use a mutex here, but this should be sufficient.
+  static sig_atomic_t creation_gate;
+  // static std::atomic_flag creation_gate;
+};
+

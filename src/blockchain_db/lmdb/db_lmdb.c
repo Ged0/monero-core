bacfaa7ab3da94338ec92cd92db35e80bfa28e92
LINE <769483043@qq.com>
2018-10-01 19:11:56 +0800

    fix

--- src/blockchain_db/lmdb/db_lmdb.c
+++ src/blockchain_db/lmdb/db_lmdb.c
@@ -7,8 +7,7 @@ void lmdb_open(BlockchainLMDB *lmdb, const char* filename, const int db_flags) {
     int result;
     int mdb_flags = MDB_NORDAHEAD;
 
-    
-    if (lmdb->db->m_open) {
+    if (lmdb != NULL && lmdb->db->m_open) {
         /* code */
         printf("Attempted to open db, but it's already open");
         return;
